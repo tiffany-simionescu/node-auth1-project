c7dd4d6829dc972a4236c41d4c591996a12aff3f
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-01-30 23:17:51 -0500

    Reformated Files, Met MVP for Part 2

--- api/api-router.js
+++ api/api-router.js
@@ -1,12 +1,8 @@
-const bcrypt = require('bcryptjs');
 const router = require('express').Router();
-const Users = require('../users/users-model');
+const bcrypt = require('bcryptjs');
+const restricted = require('./restricted-middleware');
 
-// const authRouter = require('../auth/auth-router');
-const usersRouter = require('../users/users-router');
-
-// router.use('/auth', authRouter);
-router.use('/users', usersRouter);
+const Users = require('./api-model');
 
 // GET - /api
 router.get('/', (req, res) => {
@@ -14,30 +10,54 @@ router.get('/', (req, res) => {
 })
 
 // POST - /api/register
-router.post('/register', (req, res) => {
+router.post('/register', (req, res, next) => {
   let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
 
   Users.add(user)
     .then(newUser => {
       res.status(201).json(newUser);
     })
     .catch(err => {
-      res.status(500).json(err);
+      next(err);
     })
 })
 
 // POST - /api/login
-router.post('/login', async (req, res) => {
+// router.post('/login', async (req, res, next) => {
+//   let { username, password } = req.body;
+//   const user = await Users.findBy({ username }).first();
+//   const passwordValid = await bcrypt.compare(password, user.password);
+
+//   Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//       if (user && passwordValid) {
+//         res.status(200).json({
+//           message: `Welcome ${user.username}!`
+//         })
+//       } else {
+//         res.status(401).json({
+//           message: "Invalid Credentials"
+//         })
+//       }
+//     })
+//     .catch(err => {
+//       next(err);
+//     })
+// })
+router.post('/login', (req, res, next) => {
   let { username, password } = req.body;
-  const user = await Users.findBy({ username }).first();
-  const passwordValid = await bcrypt.compare(password, user.password);
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user && passwordValid) {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
+
         res.status(200).json({
-          message: `Welcome ${user.username}!`
+          message: `Welcome ${user.username}!`,
         })
       } else {
         res.status(401).json({
@@ -46,8 +66,32 @@ router.post('/login', async (req, res) => {
       }
     })
     .catch(err => {
-      res.status(500).json(err);
+      next(err);
     })
+})
+
+// GET - /api/users
+router.get('/users', restricted(), (req, res, next) => {
+  Users.find()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(err => {
+      next(err);
+    })
+})
+
+// GET - /api/logout
+router.get('/logout', restricted(), (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) {
+      next(err);
+    } else {
+      res.json({
+        message: "You are logged out."
+      })
+    }
+  })
 })
 
 module.exports = router;
